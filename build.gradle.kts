a23b54eae9a7480841c8021bd369e66b2c5ce5ef
Strokkur24 <strokkur.24@gmail.com>
2026-04-08 01:29:53 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+  id("io.papermc.paperweight.patcher") version "2.0.0-beta.21"
+}
+
+paperweight {
+  upstreams.paper {
+    ref = providers.gradleProperty("paperRef")
+
+    patchFile {
+      path = "paper-server/build.gradle.kts"
+      outputFile = file("tabletop-server/build.gradle.kts")
+      patchFile = file("tabletop-server/build.gradle.kts.patch")
+    }
+    patchFile {
+      path = "paper-api/build.gradle.kts"
+      outputFile = file("tabletop-api/build.gradle.kts")
+      patchFile = file("tabletop-api/build.gradle.kts.patch")
+    }
+    patchDir("paperApi") {
+      upstreamPath = "paper-api"
+      excludes = setOf("build.gradle.kts")
+      patchesDir = file("tabletop-api/paper-patches")
+      outputDir = file("paper-api")
+    }
+  }
+}
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+subprojects {
+  apply(plugin = "java-library")
+
+  extensions.configure<JavaPluginExtension> {
+    toolchain {
+      languageVersion = JavaLanguageVersion.of(25)
+    }
+  }
+
+  repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl)
+  }
+
+  tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+  }
+  tasks.withType<JavaCompile> {
+    options.encoding = Charsets.UTF_8.name()
+    options.release = 25
+    options.isFork = true
+  }
+  tasks.withType<Javadoc> {
+    options.encoding = Charsets.UTF_8.name()
+  }
+  tasks.withType<ProcessResources> {
+    filteringCharset = Charsets.UTF_8.name()
+  }
+  tasks.withType<Test> {
+    testLogging {
+      showStackTraces = true
+      exceptionFormat = TestExceptionFormat.FULL
+      events(TestLogEvent.STANDARD_OUT)
+    }
+  }
+}
